55fa5ba12a895225872532901ec77f376c52885a
Nguyễn Anh Tuấn <113050646+Anhtuan2710web@users.noreply.github.com>
2023-03-08 21:44:52 +0700

    upload

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'Chúc Mừng ngày 8-03';
 const titleCard = 'Tặng người thân thương';
 const contentCard = 'Chúc Mẹ và các Chị của em 8/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mọi người trong hôm nay và cả những ngày sau';
 
-const giftImage = 'gd.png';
+const giftImage = 'gd1.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
